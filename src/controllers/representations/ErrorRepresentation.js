5c6244e08b1fc998fb48cdec2d6c349c16eae85a
Robert Dubecky <rodubecky@paypal.com>
2022-02-25 11:31:02 -0800

    added better error representation handling

---
+++ src/controllers/representations/ErrorRepresentation.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ErrorRepresentation = void 0;
+class ErrorRepresentation {
+    constructor(error) {
+        this.error = error;
+    }
+}
+exports.ErrorRepresentation = ErrorRepresentation;
